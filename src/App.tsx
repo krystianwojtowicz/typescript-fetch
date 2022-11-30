98adb9c8358ddc914a31b6a9608fd64c718189da
Krystian <krystian1wojtowicz@gmail.com>
2022-11-30 18:52:00 +0100

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState, useEffect }  from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+   ( async () => {
+    const res = await fetch('https://swapi.dev/api/people/1');
+    const data = await res.json();
+    console.log(data);
+  })()
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ssss
     </div>
   );
 }
