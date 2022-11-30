4ee188a43f4cf432049dd14615ca601b95de8d42
Krystian <krystian1wojtowicz@gmail.com>
2022-11-30 22:32:43 +0100

    picture

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,4 +1,34 @@
+import { useState, useEffect } from "react";
+
 function Photo() {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`https://picsum.photos/534/383`);
+      // const data = await response.json()
+      // console.log(data)
+
+      // const data = await response.json()
+      setImage(response.url);
+      // setImages(data)
+    })();
+
+    // fetchImages()
+  }, []);
+  // useEffect(() => {
+  //   const fetchImages = async () => {
+  //     const response = await fetch(
+  //       `https://.picsum.photos/534/383`
+  //     )
+  //     const data = await response.json()
+  //     console.log(data)
+  //     setImages(data)
+  //   }
+
+  //   fetchImages()
+  // }, [])
+
   return (
     <>
       <header>
@@ -11,14 +41,21 @@ function Photo() {
         </div>
       </header>
       <div className="picture">
-        <div className="inner-picture"></div>
+        {image ? (
+          <img className="inner-picture" src={image} alt="" />
+        ) : (
+          <div className="inner-picture"></div>
+        )}
+
+        {/* <div className="inner-picture">
+          {image && <img classN src={image} alt="" />}
+        </div> */}
         <h1>Name</h1>
         <i className="fa-solid fa-circle-check"></i>
         <div className="look">
           <span>age:</span>
           <span>eye color:</span>
         </div>
-        {/* <img src="s.jpg" alt=""> */}
       </div>
       <button>
         <span>next profiles</span>
